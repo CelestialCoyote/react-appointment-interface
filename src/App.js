2df256dc789e5b682ddbb07317d5f5e7b181827d
Paul Stearns <celestialcoyote@gmail.com>
2023-01-11 15:38:41 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AppointmentInterface from "./components/AppointmentInterface/AppointmentInterface";
+
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <AppointmentInterface />
+        </div>
+    );
+};
+
+
+export default App;
